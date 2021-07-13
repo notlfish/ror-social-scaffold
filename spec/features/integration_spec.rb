f8275135aab1aef0fca25c888f04b69b9318ffec
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-07-13 17:44:44 +0100

    add passing and failing sign up

--- spec/features/integration_spec.rb
+++ spec/features/integration_spec.rb
@@ -9,6 +9,33 @@ RSpec.describe 'Freature test' do
                        password_confirmation: '123456')
   end
 
+  describe 'Sign up process', type: :feature do
+    it ' Signs up a user' do
+      visit 'users/sign_up'
+      within('#new_user') do
+        fill_in 'Name', with: 'mike'
+        fill_in 'Email', with: 'mike@example.com'
+        fill_in 'Password', with: '123456'
+        fill_in 'Password confirmation', with: '123456'
+      end
+      click_button 'Sign up'
+      expect(page).to have_content 'Welcome! You have signed up successfully.'
+    end
+
+ 
+    it ' Signs up a user' do
+      visit 'users/sign_up'
+
+      click_button 'Sign up'
+      expect(page).to have_content "Name can't be blank"
+      expect(page).to have_content "Email can't be blank"
+      expect(page).to have_content "Password can't be blank"
+    end
+  end
+
+
+
+
   describe 'log in process', type: :feature do
     it 'logs in' do
       visit 'users/sign_in'
