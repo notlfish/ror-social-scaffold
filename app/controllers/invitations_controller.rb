102cc0d5291db6fce197b98a00c3c4f98dbaedd6
Lucas Ferrari Soto <lucasmfsoto@gmail.com>
2021-07-14 14:56:50 -0300

    Update invitations code to match the database change.
    Updated controller methods to always create, update and destroy
    invitations with user_id and friend_id interchaged as pairs.
    Add tests to the invitations model.

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,28 +1,49 @@
 class InvitationsController < ApplicationController
   def create
-    invitation = Invitation.new(user_id: params[:user], friend_id: params[:friend], confirmed: false)
-    if invitation.save
-      redirect_to User.find(invitation.friend_id),
+    invitations = [Invitation.new(user_id: params[:user],
+                                  friend_id: params[:friend],
+                                  confirmed: false,
+                                  inviter_id: params[:user]),
+                   Invitation.new(user_id: params[:friend],
+                                  friend_id: params[:user],
+                                  confirmed: false,
+                                  inviter_id: params[:user])]
+    if invitations.all?(&:save)
+      redirect_to User.find(invitations[0].friend_id),
                   notice: 'Your friendship invitation was successfully sent.'
     else
-      render
+      render :back, alert: invitations.reduce do |acc, inv|
+        acc + inv.errors.full_messages
+      end
     end
   end
 
+  ## Accepting the invitation
   def update
-    invitation = Invitation.find(params[:id])
-    user = User.find(invitation.user_id)
-    invitation.confirmed = true
-    if invitation.save
-      redirect_to user, notice: "You are now friends with #{user.name}!"
+    invitations = fetch_invitations(params[:id])
+    invitations.each { |inv| inv.confirmed = true }
+
+    if invitations.all?(&:save)
+      redirect_to user, notice: 'You now have one more friend!'
     else
-      render current_user
+      render current_user, alert: invitations.reduce do |acc, inv|
+        acc + inv.errors.full_messages
+      end
     end
   end
 
   def destroy
-    invitation = Invitation.find(params[:id])
-    invitation.destroy
+    invitations = fetch_invitations(params[:id])
+    invitations.each(&:destroy)
     redirect_to current_user, notice: 'Invitation successfully rejected.'
+  end
+
+  private
+
+  def fetch_invitations(example_id)
+    from_invitation = Invitation.find(example_id)
+    from_id = from_invitation.user_id
+    to_id = from_invitation.friend_id
+    Invitation.between(from_id, to_id)
   end
 end
--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -5,4 +5,9 @@ class Invitation < ApplicationRecord
 
   validates :user_id, uniqueness: { scope: :friend_id, message: 'You have already sent a request to this user' }
   validates :inviter_id, presence: true
+
+  scope :between, lambda { |user_id, friend_id|
+                    where('user_id = ? and friend_id = ?', user_id, friend_id)
+                      .or(where('user_id = ? and friend_id = ?', friend_id, user_id))
+                  }
 end
