b16daf82a573bf6fd15bd30933089a55e2ef64fd
Andres Castañeda <af.castaneda223@gmail.com>
2021-11-09 19:01:46 -0300

    add POST endpoints for comments and posts

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -11,7 +11,25 @@ module Api
       end
 
       def show
-        respond_with User.find(params[:user_id]).posts.sort[params[:post_id].to_i - 1].comments.sort[params[:id].to_i - 1]
+        respond_with Comment.where(params[:user_id, :post_id])
+      end
+
+      def create
+        @comment = Comment.new(comment_params)
+        @comment.post_id = params[:post_id]
+        @comment.user = current_user
+
+        if @comment.save
+          render json: @comment
+        else
+          render error: { error: 'unable to create comment.' }, status: 400
+        end
+      end
+
+      private
+
+      def comment_params
+        params.require(:comment).permit(:content)
       end
     end
   end
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -13,6 +13,22 @@ module Api
       def show
         respond_with Post.where(user_id: params[:user_id]).sort[params[:id].to_i - 1]
       end
+
+      def create
+        @post = current_user.posts.new(post_params)
+
+        if @post.save
+          render json: @post
+        else
+          render error: { error: 'unable to create post.' }, status: 400
+        end
+      end
+
+      private
+
+      def post_params
+        params.require(:post).permit(:content)
+      end
     end
   end
 end
